29f990f5101029d872923a95f09050cd934b7731
caryp <cary@rightscale.com>
2011-07-14 23:18:16 -0700

    [sys_firewall] re-factored iptables logic out of rs_utils. removed unnecessary LWRP code.

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+if node[:sys_firewall][:enabled] == "enabled" 
+  include_recipe "iptables"
+  rs_utils_firewall_rule "22" # SSH
+  rs_utils_firewall_rule "80" # HTTP
+  rs_utils_firewall_rule "443" # HTTPS
+else
+  service "iptables" do
+    supports :status => true 
+    action [:disable, :stop]
+  end
+end
